22ffaa6a8b77c35f5d8b6801e5dfef0283c1bd4d
Nia-bald <niaredoyom666@gmail.com>
2025-07-17 01:54:50 +0530

    added simple ass ranking algorithm

--- data_collector/ranking_algorithm.py
+++ data_collector/ranking_algorithm.py
@@ -1,4 +1,11 @@
+from abc import ABC, abstractmethod
+from utils.units import Task
+
 class RankingAlgorithm:
     """Ranks classified posts for selection."""
-    def rank(self, task):
-        return task  # Logic to rank posts 
+    def rank(self, task: Task):
+
+        for subreddit, processed_reddit_data in task.processed_reddit_data.items():
+            processed_reddit_data['rank'] = range(1, len(processed_reddit_data) + 1)
+            task.processed_reddit_data[subreddit] = processed_reddit_data
+        return task  # Logic to rank posts
